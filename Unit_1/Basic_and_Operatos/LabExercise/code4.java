a56184aaf27244b91fb03178f48b6a2ac59c4e49
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-15 00:15:31 +0530

    unit-I

---
+++ Unit_1/Basic_and_Operatos/LabExercise/code4.java
@@ -0,0 +1,21 @@
+package Unit_1.Basic_and_Operatos.LabExercise;
+//Change Position
+import java.util.*;
+
+class code4 {
+     public static void main(String args[]) {
+          int x, y, l;
+
+          Scanner s = new Scanner(System.in);
+
+          x = s.nextInt();
+          y = s.nextInt();
+          l = s.nextInt();
+
+          x = x + (l / 2);
+          y = y + (l / 2);
+          System.out.println(x + " " + y);
+
+          s.close();
+     }
+}
