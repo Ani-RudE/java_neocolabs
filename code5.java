5522c32ddafbbcb76634ed2b91832dc96b0f7053
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-15 11:21:27 +0530

    unit-I

---
+++ code5.java
@@ -0,0 +1,19 @@
+
+// Write a program to convert Celsius to Fahrenheit formula, F = 1.8 C+32.
+import java.util.Scanner;
+
+class code5 {
+
+     public static void main(String[] args) {
+          double f, c;
+
+          Scanner s = new Scanner(System.in);
+
+          c=s.nextInt();
+
+          f=1.8*c+32;
+          System.out.println(f);
+
+          s.close();
+     }
+}
