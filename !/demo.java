9139b192e264a29a0a5e00a95e1526aa6176ee28
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-15 11:28:05 +0530

    demo

--- !/demo.java
+++ !/demo.java
@@ -3,12 +3,13 @@ import java.util.*;
 class demo {
    public static void main(String[] args) {
 
+      int n;
       Scanner scan=new Scanner(System.in);
+      n=scan.nextInt();
 
-      for (int i = 0; i < args.length; i++)
+      for (int i=0; i<n; i++)
       {  
-         args[i]=scan.next();
-         System.out.print(args[i]+" ");
+         System.out.println();
       }
       
       System.out.println();
