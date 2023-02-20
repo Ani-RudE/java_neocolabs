b482f99ad7cadff1f86b858447c48f2fe39e083d
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-20 21:57:56 +0530

    hello

--- !/hello.java
+++ !/hello.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
-class demo {
+class hello{
    public static void main(String[] args) {
 
       int n;
       Scanner scan=new Scanner(System.in);
       n=scan.nextInt();
+      
+      scan.close();
 
+      System.out.println();
+      
       for (int i=0; i<n; i++)
       {  
          System.out.println();
       }
-      
-      System.out.println();
-
-      scan.close();
    }
 }
