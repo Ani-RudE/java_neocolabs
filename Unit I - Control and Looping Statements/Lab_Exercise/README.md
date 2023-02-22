1c7ec9c741f29204a25d78cce1af90d2a65a63a4
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-22 14:26:26 +0530

    unit-I

---
+++ Unit I - Control and Looping Statements/Lab_Exercise/README.md
@@ -0,0 +1,13 @@
+#
+#code1 - Marks and Grades.
+
+#code2 - IPL Season and Preity's favourite team.
+
+#code3 - 
+
+#code4 - 
+
+#code5 - 
+
+#code6 - 
+#
--- Unit I - Control and Looping Statements/Lab_Exercise/code2.java
+++ Unit I - Control and Looping Statements/Lab_Exercise/code2.java
@@ -1,32 +1,144 @@
 // IPL Season and Preity's favourite team.
-import java.util.*;
+import java.util.Scanner;
 
 class hello{
-     public static void main(String args[])
+     public static void main (String[] args)
      {
-          int mark;
-          Scanner scan =new Scanner(System.in);
-          mark=scan.nextInt();
-          
-          if(mark>=85)
+          Scanner scan = new Scanner(System.in) ;
+          int day = scan.nextInt();
+          int month =scan.nextInt();
+          String sign="";
+
+          if((day<1) ||(day>31) || (month<1) ||(month>12))
           {
-               System.out.print("A");
-          }
-          else if(mark>=75)
-          {
-               System.out.print("B");
-          }
-          else if(mark>=65)
-          {
-               System.out.print("C");
-          }
-          else if(mark>=0)
-          {
-               System.out.print("D");
+               System.out.print("Invalid Date/Month");
           }
           else
           {
-               System.out.print("Invalid");
+               if (month==1)
+               {
+                    if (day<20)
+                    sign="Capricorn";
+               }
+               else
+               {
+                    sign="Aquarius";
+               } 
+               if (month==2)
+               {
+                    if (day<19)
+                    {
+                         sign="Aquarius";
+                    }
+               }
+               else
+               {
+                    sign="Pisces";
+               } 
+               if (month==3)
+               {
+                    if (day<21)
+                    sign="Pisces";
+               }
+               else
+               {
+                    sign="Aries";
+               }
+               if (month==4)
+               {
+                    if (day<20)
+                    sign="Aries";
+               }
+               else
+               {
+                    sign="Taurus";
+               }
+               if (month==5)
+               {
+                    if (day<21)
+                    {
+                         sign = "Taurus";
+                    }
+               }
+               else
+               {
+                    sign = "Gemini";
+               }
+               if (month==6)
+               {
+                    if (day < 21)
+                    {
+                         sign = "Gemini";
+                    }
+               }
+               else
+               {
+                    sign = "Cancer";
+               }
+               if (month == 7)
+               {
+                    if (day < 23)
+                    {
+                         sign = "Cancer";
+                    }
+               }
+               else
+               {
+                    sign = "Leo";
+               }
+               if( month == 8)
+               {
+                    if (day < 23)
+                    {
+                         sign = "Leo";
+                    }
+               }
+               else
+               {
+                    sign = "Virgo";
+               }
+               if (month == 9)
+               {
+                    if (day < 23)
+                    {
+                         sign = "Virgo";
+                    }
+               }
+               else
+               {
+                    sign = "Libra";
+               }
+               if (month == 10)
+               {
+                    if (day < 23)
+                    {
+                         sign = "Libra";
+                    }
+               }
+               else
+               {
+                    sign = "Scorpio";
+               } 
+               if (month == 11)
+               {
+                    if (day < 22)
+                    sign = "scorpio";
+               }
+               else
+               {
+                    sign = "Sagittarius";
+               }
+               if (month == 12)
+               {
+                    if (day < 22)
+                    sign = "Sagittarius";
+               }
+               else
+               {
+                    sign ="Capricorn";
+               }
+
+               System.out.print("Astrological sign for " + day + "-" + month + " is " + sign );
           }
 
           scan.close();
