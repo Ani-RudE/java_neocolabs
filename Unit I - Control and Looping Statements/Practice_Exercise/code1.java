35c082cbaa13551890c7ec0204e9d07b9354b4b0
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-26 00:46:45 +0530

    unit-I

--- Unit I - Control and Looping Statements/Practice_Exercise/code1.java
+++ Unit I - Control and Looping Statements/Practice_Exercise/code1.java
@@ -1,9 +1,40 @@
-//
-// import java.util.*;
+//Ticket Types.
 
-public class code1{
-     public static void main(String[] args)
-     {
-          
-     }
+import java.util.*;
+
+class hello{
+    public static void main(String[] args)
+    {
+        char tick;
+        Scanner scan=new Scanner(System.in);
+        tick=scan.next().charAt(0);
+        
+        switch(tick)
+        {
+            case 'E':
+            case 'e':
+                System.out.println("Early Bird Ticket");
+                break;
+            case 'D':
+            case 'd':
+                System.out.println("Discount Ticket");
+                break;
+            case 'V':
+            case 'v':
+                System.out.println("VIP Ticket");
+                break;
+            case 'S':
+            case 's':
+                System.out.println("Standard Ticket");
+                break;
+            case 'C':
+            case 'c':
+                System.out.println("Children Ticket");
+                break;
+            default:
+                System.out.println("Invalid Case!");
+        }
+        
+        scan.close();
+    }
 }
