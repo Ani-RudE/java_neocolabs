8e399f3dfad216667c722a49a4558a8b655d6a02
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-03-13 11:52:41 +0530

    java

---
+++ 5. Unit I - Inheritance/Lab_Exercise/code1.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+interface Rotate {
+     void leftRotate(int arr[], int d, int n);
+
+     void leftRotatebyOne(int arr[], int n);
+
+     void printArray(int arr[], int n);
+}
+
+class Main implements Rotate {
+     public void leftRotate(int arr[], int d, int n) {
+          for (int i = 0; i < d; i++)
+               leftRotatebyOne(arr, n);
+     }
+
+     public void leftRotatebyOne(int arr[], int n) {
+          int i, temp;
+          temp = arr[0];
+          for (i = 0; i < n - 1; i++)
+               arr[i] = arr[i + 1];
+          arr[i] = temp;
+     }
+
+     /* utility function to print an array */
+     public void printArray(int arr[], int n) {
+          for (int i = 0; i < n; i++)
+               System.out.print(arr[i] + " ");
+     }
+
+     public static void main(String[] args) {
+          Main rotate = new Main();
+          int n;
+          Scanner scan = new Scanner(System.in);
+          n = scan.nextInt();
+          int arr[] = new int[n];
+          for (int i = 0; i < n; i++) {
+               arr[i] = scan.nextInt();
+          }
+          rotate.leftRotate(arr, 1, n);
+          rotate.printArray(arr, n);
+
+          scan.close();
+     }
+}
