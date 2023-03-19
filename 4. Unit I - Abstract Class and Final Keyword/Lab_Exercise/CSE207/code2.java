8adb95b460b2c0105c3971668e355b4c10d74939
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-03-19 18:27:30 +0530

    mod4

--- 4. Unit I - Abstract Class and Final Keyword/Lab_Exercise/CSE207/code2.java
+++ 4. Unit I - Abstract Class and Final Keyword/Lab_Exercise/CSE207/code2.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.*
 
 interface AdvancedArithmetic{
     int divisorSum(int n);
--- 4. Unit I - Abstract Class and Final Keyword/Lab_Exercise/CSE207/code3.java
+++ 4. Unit I - Abstract Class and Final Keyword/Lab_Exercise/CSE207/code3.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.*
 
 interface Food{
     public String getType();
