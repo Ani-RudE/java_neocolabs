ae95e39f74ff08dd225d24c6832f859e04002df3
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-04-12 23:26:32 +0530

    mod10

--- 10. Unit 2 - Threads/Lab Exercise/2. CSE207_12_2/README.md
+++ 10. Unit 2 - Threads/Lab Exercise/2. CSE207_12_2/README.md
@@ -1,6 +1,5 @@
 #
 
 - code1 - Complete the code snippet
-## The code will give an error in any state, beacuse there's an extra "}" given in the footer snippet that can not be removed [-_-]
 
 #
--- 10. Unit 2 - Threads/Lab Exercise/2. CSE207_12_2/code1.java
+++ 10. Unit 2 - Threads/Lab Exercise/2. CSE207_12_2/code1.java
@@ -1,17 +1,4 @@
-//Complete the code snippet
-class Main {
-     public static void main(String args[]) {
-          Table obj = new Table();// only one object
-          MyThread1 t1 = new MyThread1(obj);
-          MyThread2 t2 = new MyThread2(obj);
-          t1.start();
-          t2.start();
-     }
-}
-
-class Table {
-     void printTable(int n) {// synchronized method
-          synchronized (this) {
+synchronized (this) {
                for (int i = 1; i <= 5; i++) {
                     System.out.println(n * i);
                     try {
@@ -46,4 +33,3 @@ class MyThread2 extends Thread {
      public void run() {
           t.printTable(101);
      }
-}
