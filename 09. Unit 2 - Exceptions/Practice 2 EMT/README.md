2970becda6a706aaccbcf35f43645c249e5ba6b3
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-04-14 00:14:42 +0530

    mod9

---
+++ 09. Unit 2 - Exceptions/Practice 2 EMT/README.md
@@ -0,0 +1,13 @@
+#
+
+- code1 - Write a program to obtain two numbers and print their quotient
+
+- code2 - 
+
+- code3 - 
+
+- code4 - 
+
+- code5 - 
+
+#
---
+++ 09. Unit 2 - Exceptions/Practice 2 EMT/code1.java
@@ -0,0 +1,24 @@
+//Write a program to obtain two numbers and print their quotient
+import java.util.*;
+
+class Q01Simple_List {
+     public static void main(String[] args) throws java.lang.Exception {
+          Scanner scan = new Scanner(System.in);
+          // Enter dividend
+          try {
+               int dividend = scan.nextInt();
+               // Enter divisor
+               int divisor = scan.nextInt();
+
+               try {
+                    System.out.println(dividend / divisor);
+               } catch (ArithmeticException e) {
+                    System.out.println(e);
+               }
+          } catch (Exception e) {
+               System.out.println(e);
+          }
+
+          scan.close();
+     }
+}
